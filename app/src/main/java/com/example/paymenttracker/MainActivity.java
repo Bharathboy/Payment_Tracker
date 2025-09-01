7504e678940c56a4deac1e9375076f558634c5b8
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2025-09-01 10:57:49 +0530

    kk

--- app/src/main/java/com/example/paymenttracker/MainActivity.java
+++ app/src/main/java/com/example/paymenttracker/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+        }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
