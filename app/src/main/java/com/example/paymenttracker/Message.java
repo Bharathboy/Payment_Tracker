b00faf5f6f62221179f5fd9a020cb0da84d597a2
Bharath s <84450562+Bharathboy@users.noreply.github.com>
2025-09-01 12:01:08 +0530

    ok

--- app/src/main/java/com/example/paymenttracker/Message.java
+++ app/src/main/java/com/example/paymenttracker/Message.java
@@ -1,7 +1,10 @@
 
 package com.example.paymenttracker;
 
-public class Message {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Message implements Parcelable {
     public String sender;
     public String content;
     public String status;
@@ -13,4 +16,37 @@ public class Message {
         this.status = status;
         this.date = date;
     }
+
+    // Parcelable implementation
+    protected Message(Parcel in) {
+        sender = in.readString();
+        content = in.readString();
+        status = in.readString();
+        date = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(sender);
+        dest.writeString(content);
+        dest.writeString(status);
+        dest.writeString(date);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Message> CREATOR = new Creator<Message>() {
+        @Override
+        public Message createFromParcel(Parcel in) {
+            return new Message(in);
+        }
+
+        @Override
+        public Message[] newArray(int size) {
+            return new Message[size];
+        }
+    };
 }
